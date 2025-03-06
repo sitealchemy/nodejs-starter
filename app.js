b8b48e41c93ed38077b34cc87335155686367e9e
Grzegorz Serwin <grzegorz.serwin@gmail.com>
2025-03-06 23:29:08 +0100

    Add clear text button and update heading in index.html

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
+
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });

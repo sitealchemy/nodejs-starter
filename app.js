fe115a0ae60719de3a9e109317cf654487290a5a
Grzegorz Serwin <grzegorz.serwin@gmail.com>
2025-03-06 22:31:23 +0100

    hello fix

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Use the router for handling routes
 app.use('/', indexRouter);
 
-// Catch-all route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
-
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
+
+// Catch-all route for handling 404 errors
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
